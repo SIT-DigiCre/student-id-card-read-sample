980bb6dec23fe337dcc81531bd42a1535183e8e8
Kaoru Chisen <cordx56@cordx.net>
2021-04-09 21:27:33 +0900

    use Timer in C# script

--- csharp/SITStudentIDLoggerConsole/Program.cs
+++ csharp/SITStudentIDLoggerConsole/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Timers;
 
 namespace SITStudentIDLoggerConsole
 {
@@ -9,31 +10,43 @@ namespace SITStudentIDLoggerConsole
         {
             Console.WriteLine("SIT Student ID Logger C# Console");
             Console.WriteLine("Copyright (c) 2021 cordx56");
+            Reader r = new Reader();
+            Timer t = new Timer(100);
+            t.Elapsed += r.read;
+            t.Enabled = true;
+            while (true)
+            {
+            }
+        }
+    }
+
+    class Reader
+    {
+        private string beforeReadStudentID;
+        public void read(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            Timer t = (Timer)sender;
+            t.Enabled = false;
             try
             {
                 using (SITStudentID.Reader r = new SITStudentID.Reader())
                 {
-                    string beforeReadStudentID = "";
-                    while (true)
+                    try
                     {
-                        try
+                        SITStudentID.Card card = r.Read();
+                        if (card.ID != beforeReadStudentID)
                         {
-                            SITStudentID.Card card = r.Read();
-                            if (card.ID != beforeReadStudentID)
+                            Console.WriteLine(card.ID);
+                            using (StreamWriter outputFile = File.AppendText("ids.txt"))
                             {
-                                Console.WriteLine(card.ID);
-                                // Console.Write(card.ValidFrom.ToString() + " -> " + card.ValidTo.ToString());
-                                using (StreamWriter outputFile = File.AppendText("ids.txt"))
-                                {
-                                    outputFile.WriteLine(card.ID);
-                                }
-                                beforeReadStudentID = card.ID;
+                                outputFile.WriteLine(card.ID);
                             }
+                            beforeReadStudentID = card.ID;
                         }
-                        catch (Exception ex)
-                        {
-                            // Console.WriteLine(ex.Message);
-                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        // Console.WriteLine(ex.Message);
                     }
                 }
             }
@@ -41,6 +54,7 @@ namespace SITStudentIDLoggerConsole
             {
                 Console.WriteLine(ex.Message);
             }
+            t.Enabled = true;
         }
     }
 }
